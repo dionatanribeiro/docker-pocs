0fcd622059c13cd0390daa65ec56e2817e0d89b8
Dionatan Ribeiro <dionatangil@gmail.com>
2017-12-03 23:53:27 -0200

    testes com Dockerfile

---
+++ node-mongo-compose/backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const restful = require('node-restful')
+const server = express()
+const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// Database
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://arrozdb/mydb')
+
+// Middlewares
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true }
+})
+
+// REST API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+// Routes
+Client.register(server, '/clients')
+
+// Start Server
+server.listen(3000)
